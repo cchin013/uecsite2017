1ee9fe2f994fa4d0992f9e85b106af9d729cafa4
Jimmy <jitani@ucsd.edu>
2017-08-18 09:20:33 +0800

    ui fixes on homepage

--- cache/_virtuemart/e682b6626f3b680ba56c31fc3654d5b6-cache-_virtuemart-9962488d30ae39923dfca11d7d7e3b0e.php
+++ cache/_virtuemart/e682b6626f3b680ba56c31fc3654d5b6-cache-_virtuemart-9962488d30ae39923dfca11d7d7e3b0e.php
@@ -1,1 +1,1 @@
-<?php die("Access Denied"); ?>#x#O:8:"stdClass":2:{s:5:"route";s:0:"";s:6:"Itemid";s:3:"569";}
+<?php die("Access Denied"); ?>#x#O:8:"stdClass":2:{s:5:"route";s:7:"404/404";s:6:"Itemid";s:3:"544";}
